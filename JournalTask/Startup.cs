d5911fc84355dae08eb17b293defa8fa171e8f9d
saher sayed abdelaziz <SaherSayed@cis.asu.edu.eg>
2020-07-20 01:44:27 +0200

    add files

---
+++ JournalTask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JournalTask.Startup))]
+namespace JournalTask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
