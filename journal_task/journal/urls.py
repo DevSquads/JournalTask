05775636429326fe7309e45e527c55f49806e02a
Khloud Khalid <khloud.khalid7@gmail.com>
2020-07-23 18:00:38 +0200

    Created Different Viewing Modes For Different Access Levels

--- journal_task/journal/urls.py
+++ journal_task/journal/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
 
 urlpatterns = [
-    path('', views.ArticleList.as_view(), name='index'),
-    path("register/", views.register, name="register"),
+    path('', views.ArticleList.as_view(), name='index'),    
+    url(r'^register/$', views.register, name='register'),
 ]
--- journal_task/journal/views.py
+++ journal_task/journal/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 from django.views import generic
 from journal.models import Article
 from django.utils.decorators import method_decorator
+from django.db.models import Count,Q
+from django.db.models import Case, When
 
 def register(response):
 	if response.method == "POST":
@@ -22,23 +24,43 @@ def register(response):
 	return render(response, "journal/register.html", {"form":form})
 
 def login(request):
-	username = request.POST['username']
-	password = request.POST['password']
-	user = authenticate(request, username=username, password=password)
-	if user is not None:
-		login(request, user)
-		return redirect("/journal")
-	else:
-		# Return an 'invalid login' error message.
-		...
+	if request.method == "POST":
+		if request.POST.get('submit') == 'sign_in':
+			username = request.POST['username']
+			password = request.POST['password']
+			user = authenticate(request, username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return redirect("/journal")
+			else:
+				return redirect("/journal/register")
 
-@method_decorator(login_required, name='dispatch')
-class ArticleList(generic.TemplateView):
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class AdminArticleList(generic.TemplateView):
 	template_name = "article_list.html"
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		context['object_list'] = Article.objects.all()
+
+		context['object_list'] = Article.objects.annotate(count_approved=Count('author', filter=Q(approved=True))).order_by('count_approved')
 		return context
+
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class AuthorArticleList(generic.TemplateView):
+	template_name = "article_list.html"
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		context['object_list'] = Article.objects.order_by(
+    Case(When(author=self.request.user.id, then=0), default=1))
+		return context
+
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class ArticleList(generic.View):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            return AdminArticleList.as_view()(request, *args, **kwargs)
+        else:
+            return AuthorArticleList.as_view()(request, *args, **kwargs)
 
 @login_required
 def create_article(request):
