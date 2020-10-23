bec81bb5d47540346c47be6dbd162f3508efe5ca
Ahmed-10 <ahmedalaa.10@hotmail.com>
2020-10-23 03:48:09 +0200

    add: database schema, SQLAlchemy model for the database tables, and connect to the database from the app

---
+++ flaskr/__init__.py
@@ -0,0 +1,90 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+database_uri = 'postgresql://postgres:root@localhost:5432/test'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+db = SQLAlchemy(app)
+
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True)
+    mail = db.Column(db.String(50), nullable=False, unique=True)
+    author_name = db.Column(db.String(50), nullable=False)
+    approved_articles = db.Column(db.Integer, nullable=False)
+    articles = db.relationship('Article', backref='authors', lazy=True)
+
+    def __init__(self, mail, name, approved_articles):
+        self.mail = mail
+        self.author_name = name
+        self.approved_articles = approved_articles
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+          'id': self.id,
+          'mail': self.mail,
+          'name': self.author_name,
+          'approved_articles': self.approved_articles
+        }
+
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    approved = db.Column(db.Boolean, nullable=False)
+    author_id = db.Column(
+        db.Integer,
+        db.ForeignKey('authors.id'),
+        nullable=False
+        )
+
+    def __init__(self, title, description, approved, author_id):
+        self.title = title
+        self.description = description
+        self.approved = approved
+        self.author_id = author_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+          'id': self.id,
+          'title': self.title,
+          'description': self.description,
+          'approved': self.approved,
+          'author_id': self.author_id
+        }
+
+
+@app.route('/')
+def hello():
+    author = Author.query.first()
+    return 'Hello ' + author.author_name
+
+
+if __name__ == '__main__':
+    app.run()
