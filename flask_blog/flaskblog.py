a803154d766459ff415a0f006f32822f321223ae
RanaKhamis <ranakhms@gmail.com>
2020-12-07 00:57:25 +0200

    Adding some home and about pages

---
+++ flask_blog/flaskblog.py
@@ -0,0 +1,36 @@
+from flask import Flask, escape, request, render_template
+
+app = Flask(__name__)
+
+post= [
+    {
+        'author= Rana',
+        'title= tennis',
+        'content= Blog post 1',
+        'Date_Posted= April 21 , 2019'
+
+    }
+    {
+        'author= Radwa',
+        'title= Books',
+        'content= Blog post 2',
+        'Date_Posted= April 21 , 2019'
+
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
