f2b224aa02fbbcd3f6fd56a588b774a1ea52cbf9
RanaKhamis <ranakhms@gmail.com>
2020-12-07 16:50:38 +0200

    Adding secret key, Registration/Login forms&routes

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,8 +1,11 @@
 from flask import Flask, escape, request, render_template, url_for
-
 #url_for finds the exact route for us in flask
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'bee2b6b98e91cbe9c98f9268a8eea985'
+
 
 posts = [
     {
@@ -30,6 +33,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title=about)
+
+@app.route('/register')
+def register():
+    form= RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def register():
+    form= LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
