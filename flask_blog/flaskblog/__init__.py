f82beff74d261707e375ef776a7c18d024972f67
RanaKhamis <ranakhms@gmail.com>
2020-12-08 17:03:29 +0200

    Authenticating Users

--- flask_blog/flaskblog/__init__.py
+++ flask_blog/flaskblog/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
-#url_for finds the exact route for us in flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'bee2b6b98e91cbe9c98f9268a8eea985'
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
+
+
+
+
+
--- flask_blog/flaskblog/models.py
+++ flask_blog/flaskblog/models.py
@@ -1,8 +1,12 @@
 from datetime import datetime
-from flaskblog import db
+from flaskblog import db , login_manager
+from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model): 
+class User(db.Model, UserMixin): 
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -14,6 +18,7 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
+  
 class Post(db.Model):
     __tablename__ = 'Posts'
     id = db.Column(db.Integer, primary_key=True)
