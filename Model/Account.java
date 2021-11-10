3a27f768077c42d4f91deafac75d400ae113a482
Mirhanadham <mirhanadham@outlook.com>
2021-11-10 04:29:25 +0200

    two java packages for journal task
    Entire Model

---
+++ Model/Account.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Account {
+    String name;
+    String password;
+
+    public Account(String username,String password){
+        this.name=username;
+        this.password=password;
+    }
+}
